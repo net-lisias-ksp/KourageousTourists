696d08efa374a8f1c073dacfba2ca26e86b2c83f
Lisias T <me@lisias.net>
2022-09-17 15:51:57 -0300

    Safer KSPe handling

--- Source/KourageousTourists/Properties/AssemblyInfo.cs
+++ Source/KourageousTourists/Properties/AssemblyInfo.cs
@@ -18,8 +18,13 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion (KourageousTourists.Version.Number)]
+
+#if KSPe
 [assembly: KSPAssemblyDependency("KSPe", 2, 4)]
 [assembly: KSPAssemblyDependency("KSPe.UI", 2, 4)]
+#else
+[assembly: KSPAssemblyDependency("KSPe.Light.KT", 2, 4)]
+#endif
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
