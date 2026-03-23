54454d4e65fe573f58b5471043770793e242a34d
Lisias T <me@lisias.net>
2026-03-22 22:20:03 -0300

    Legal Mambo Jambo 2026 Edition

--- Source/KourageousTourists/Properties/LegalMamboJambo.cs
+++ Source/KourageousTourists/Properties/LegalMamboJambo.cs
@@ -1,6 +1,6 @@
 ﻿/*
 	This file is part of Kourageous Tourists /L
-		© 2020-2025 LisiasT : http://lisias.net <support@lisias.net>
+		© 2020-2026 LisiasT : http://lisias.net <support@lisias.net>
 		© 2017-2020 Nikita Makeev (whale_2)
 
 	Kourageous Tourists /L is double licensed, as follows:
@@ -27,7 +27,7 @@ namespace KourageousTourists
 	public static class LegalMamboJambo
 	{
 		public const string Company = "/L Aerospace/KSP Division";
-		public const string Copyright = "© 2020-2025 LisiasT";
+		public const string Copyright = "© 2020-2026 LisiasT";
 		public const string Product = "KourageousTourists";
 		public const string Trademark = "Kourageous Tourists™ by whale_2, LisiasT; /L™ by LisiasT";
 	}
